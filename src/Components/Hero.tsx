a3479a1c829958156ff343c4ee2d8e28cfe5d21b
Michael Abbott <abbott.michael96@gmail.com>
2023-02-11 15:40:42 +0000

    updated nav buttons to generate from state, also CSS changes

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useState } from "react";
 
 
 export function Hero() {
@@ -13,32 +13,35 @@ export function Hero() {
     counter++;
     if (counter >= text.length) {
     counter = 0;
-    
-    // clearInterval(inst); // uncomment this if you want to stop refreshing after one cycle
         }
-    }           
+    }
 
+    const [nav, setNav] = useState(["About", "Portfolio", "Contact"])
 
     return(
-        <div class="h-screen w-screen absolute  flex items-center justify-center ">
-            <div class=" bg-stone-600 sm:rounded-lg shadow h-screen sm:h-1/3 whitespace-normal  sm:grid sm:self-center">
-                    <div class="flex flex-row flex-wrap justify-items-center sm:space-x-12  pr-5 items-center">
-                        <img class="rounded-full h-28 my-10 ml-10 sm:ml-16" src='src/assets/mike.png' alt="Mike"></img>
-                        <div class="sm:space-y-10 md:space-y-8 md:pt-10 sm:-mt-10 ml-5 space-y-10 ">
+        <main className="h-screen w-screen absolute  flex items-center justify-center ">
+            <section className=" bg-stone-600 sm:rounded-lg shadow h-screen sm:h-1/3 xl:h-1/2 whitespace-normal  sm:grid sm:self-center">
+                    <div className="flex flex-row flex-wrap justify-items-center sm:space-x-12  pr-5 items-center">
+                        <img className="rounded-full h-28 my-10 ml-10 sm:ml-16" src='src/assets/mike.png' alt="Mike"></img>
+                        <div className="sm:space-y-6 xl:space-y-12 sm:-mt-1 ml-5 space-y-6 ">
                             <div>
-                                <p id ='title' class=" text-6xl xl:text-8xl text-slate-50 ">Hi!</p>
-                                <p class="text-4xl xl:text-6xl text-slate-50">I'm Michael,</p>
-                                <p class="text-4xl xl:text-6xl text-slate-50">Full Stack Developer<span class="animate-bounce inline-block">.</span></p>
+                                <p id ='title' className=" text-5xl xl:text-7xl text-slate-50 ">Hi!</p>
+                                <p className="text-4xl xl:text-6xl text-slate-50">I'm Michael,</p>
+                                <p className="text-4xl xl:text-6xl text-slate-50">Full Stack Developer
+                                    <span className="animate-bounce inline-block">.</span>
+                                </p>
                             </div>
-                            <div class="flex space-x-2">
-                                <button class="flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-slate-50 whitespace-no-wrap bg-stone-700 border-2 border-slate-50 rounded-full shadow-sm hover:bg-slate-50 hover:text-stone-700 hover:border-white">About</button>
-                                <button class="flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-slate-50 whitespace-no-wrap bg-stone-700 border-2 border-slate-50 rounded-full shadow-sm hover:bg-slate-50 hover:text-stone-700 hover:border-white">Portfolio</button>
-                                <button class="flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-slate-50 whitespace-no-wrap bg-stone-700 border-2 border-slate-50 rounded-full shadow-sm hover:bg-slate-50 hover:text-stone-700 hover:border-white">Contact</button>
+                            <div className="flex space-x-2">
+                                {nav.map((nav, idx) => (
+                                    <button key={idx} className="flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-slate-50 whitespace-no-wrap bg-stone-700 border-2 border-slate-50 rounded-full shadow-sm hover:bg-slate-50 hover:text-stone-700 hover:border-white">
+                                        {nav}
+                                    </button>    
+                                ))}
                             </div>
                         </div>
                     </div>
-                    </div>
-        </div>
+            </section>
+        </main>
                 
     )
 }
