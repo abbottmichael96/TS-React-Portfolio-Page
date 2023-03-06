4ef83e4aeb869e2e6fe6a2a754db6b01240e4ab1
Michael Abbott <abbott.michael96@gmail.com>
2023-03-06 20:40:41 +0000

    started about page structuring

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,7 +1,13 @@
 export function About() {
   return (
     <>
-      <div className="bg-stone-600 h-screen flex items-center justify-center overflow-hidden w-auto top"></div>
+      <div className="bg-stone-600 h-screen flex items-center justify-center overflow-hidden w-auto top">
+      <section className="bg-stone-700 md:w-8/12 sm:w-11/12 lg:w-7/12 w-screen sm:rounded-lg shadow h-3/4 xl:h-3/4 sm:flex justify-center -mt-36 sm:mt-0">
+      <div className="flex flex-row flex-wrap sm:space-x-12 sm:items-center sm:justify-items-centre">
+      <p className="text-4xl xl:text-2xl text-slate-50">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem maiores dicta est minus, sequi eligendi maxime ex mollitia alias et laboriosam autem sed tempora voluptates in non porro? Repellendus, doloremque!</p>
+      </div>
+      </section>
+      </div>
     </>
   );
 }
